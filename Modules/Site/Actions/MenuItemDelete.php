2c7f281cc29c2c8e67cce325f7251cc41201f282
danila k <danila k>
2017-05-06 21:58:34 +0300

    Add comments

--- Modules/Site/Actions/MenuItemDelete.php
+++ Modules/Site/Actions/MenuItemDelete.php
@@ -1,5 +1,7 @@
 <?php namespace Monstercms\Modules\site;
 
+defined('MCMS_ACCESS') or die('No direct script access.');
+
 /**
  * @var $this Core\ControllerAbstract
  */
@@ -7,18 +9,21 @@ use \Monstercms\Core;
 use \Monstercms\Core\MCMS;
 use \Monstercms\Lib;
 
+//Проверяем есть ли необходимые данные
 if ((int) $this->getObjectId() === 0){
     throw new Core\HttpErrorException(404);
 }
+
 if ((int) $this->getParam('menu_id') === 0){
     throw new Core\HttpErrorException(404);
 }
 
+//Права доступа
 if (!Core\Users::isAdmin()) {
     throw new Core\HttpErrorException(403);
 }
 
-$menuItemId = (int) $this->getObjectId();
+$menuItemId  = (int) $this->getObjectId();
 $menuId      = (int) $this->getParam('menu_id');
 
 $out = array(
@@ -27,34 +32,51 @@ $out = array(
     'message' => ''
 );
 
-if ($this->model('MenuItems')->isChilds($menuId, $menuItemId ))
-{
-    $out['message'] = Core\Lang::get('site.errorDeleteMenu');
-}
-else if ($this->model('MenuItems')->getIndexId() === $menuItemId)
-{
-    $out['message'] = Core\Lang::get('site.indexDeleteError');
-}
-else
-{
+//Если у пункта меню есть подпункты, выводим ошибку
+if ($this->model('MenuItems')->isChilds($menuId, $menuItemId )) {
+    $out['message'] = Core\Lang::get('Site.errorDeleteMenu');
+
+//Если пункт меню является индексом, выводим ошибку
+} else if ($this->model('MenuItems')->getIndexId() === $menuItemId) {
+    $out['message'] = Core\Lang::get('Site.indexDeleteError');
+
+//если удаление допустимо, удаляем пункт меню
+} else {
+    //Получаем информацию об пункте меню
     $menuItemInfo = $this->model('MenuItems')->menuItemInfo($menuItemId);
+
+    //Если информация об пункте меню пуста, выводим сообщение об ошибке
     if(!isset($menuItemInfo->module)){
         print json_encode($out);
         exit();
     }
-    $module       = $menuItemInfo->module;
+
+    $module      = $menuItemInfo->module;
     $objectId    = $menuItemInfo->object_id;
     $itemType    = $menuItemInfo->item_type;
 
-    Core\Events::cell('site.menuItemDeleteBefore', 'void', array($module, $itemType, $objectId));
+    //Вызываем событие
+    Core\Events::cell('Site.menuItemDeleteBefore', 'void', array($module, $itemType, $objectId));
 
+    //Удаляем пункт меню
     $this->model('MenuItems')->menuItemDelete($menuItemId);
 
     $out['delete'] = true;
 
-    Core\Events::cell('site.menuItemDeleteEnd', 'void',
+    //вызываем событие
+    Core\Events::cell('Site.menuItemDeleteEnd', 'void',
         array($module, $itemType, $objectId));
 }
 
+
+/*
+ Выводим данные, в формате json
+
+'id'      - ид пункта меню
+'delete'  - обвенчалась ли попытка удаления (true, false)
+'message' - сообщение об ошибке
+
+ */
 print json_encode($out);
+
 exit();
