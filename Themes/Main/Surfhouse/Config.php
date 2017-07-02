aa47986da277df8b05409f86ee69720eef5dd0e8
danila k <danila k>
2017-07-02 11:53:38 +0300

    Tama configurations

--- Themes/Main/Surfhouse/Config.php
+++ Themes/Main/Surfhouse/Config.php
@@ -1,7 +1,14 @@
 <?php
 return array(
-    'home' => array(
-        'themesName' => 'Surfhouse - главная',
+    'Home' => array(
+        'name' => 'Surfhouse - главная',
+        'file'       => 'Base.php',
+        'style'      => 'style.css',
+        'preview'    => 'preview/base.png',
+    ),
+
+    'Contact' => array(
+        'name'       => 'Surfhouse - Контакты',
         'file'       => 'Base.php',
         'style'      => 'style.css',
         'preview'    => 'preview/base.png',
